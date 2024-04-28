b62ca382288755067a15b850424ee0c40334f12d
nohjuhyeon <njh2720@gmail.com>
2024-04-28 12:35:40 +0900

    [Bronze V] Title: 구구단, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2739. 구구단/구구단.py
@@ -0,0 +1,11 @@
+num_first = int(input())
+num_second = 0
+while num_second < 9 :
+    pass
+    num_second = num_second + 1
+    
+    print("{} * {} = {}".format(num_first,num_second, num_first * num_second))
+    if num_first >=9:
+        pass
+    pass
+
