9dbdf18aa44844e394e8742116ed9042bdc67d9e
nohjuhyeon <njh2720@gmail.com>
2024-05-05 22:29:34 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,14 @@
+list_a=[]
+list_b = []
+for i in range(28):
+    list_a.append(int(input()))
+for i in range(1, 31):
+    if i in list_a: 
+        pass
+    else :
+        list_b.append(i)
+        pass
+list_b.sort
+print(list_b[0])
+print(list_b[1])
+pass
