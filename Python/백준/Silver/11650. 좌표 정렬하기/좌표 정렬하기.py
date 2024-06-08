e93f557ea1a580d71fb38b8a2d934fa14aa1ef91
nohjuhyeon <njh2720@gmail.com>
2024-06-08 14:36:51 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4140 ms, Memory: 51900 KB -BaekjoonHub

---
+++ Python/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+def question(N):
+    answer = []
+    for i in range(N):
+        A,B = map(int,input().split())
+        answer.append((A,B)) 
+    answer.sort(key = lambda x:(x[0],x[1]))
+    for i in range(len(answer)):
+        print(answer[i][0],answer[i][1])
+    return answer
+
+answer = question(N)
